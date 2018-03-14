a118aba4a3b0571ecf4f3a3c9f4f5c9c18a3d60b
himanshu-sikaria <himanshu.sikaria@socialcops.com>
2018-03-14 11:42:36 +0530

    adding espa_creds

---
+++ R/espa_creds.R
@@ -0,0 +1,13 @@
+#' Input and save esap-api Credentials
+#' @description Save the api credentials as global environment to be used in other functions in rLandsat. This is a pre-requisite for running any of the other functions requiring espa-api
+#' @param username espa account's username
+#' @param password espa account's password corresponding to the username
+#' @details If you do not have an account with espa, please create one here: https://ers.cr.usgs.gov/register
+#' @return NULL. Just saves the username and password in .Reviron
+#' @export
+#'
+#' @examples espa_creds(username = "your_espaname", password = "secret_password")
+espa_creds <- function(username, password){
+  Sys.setenv("espa_username" = username)
+  Sys.setenv("espa_password" = password)
+}
