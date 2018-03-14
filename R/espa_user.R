5cd44b4eb1978888a1f562151c02586174950d88
himanshu-sikaria <himanshu.sikaria@socialcops.com>
2018-03-14 12:19:33 +0530

    espa_user function added

---
+++ R/espa_user.R
@@ -0,0 +1,40 @@
+
+
+#' Validate espa credentials
+#' @description To check espa credentials and if espa-api is responding. Suggest to use espa_creds() function to store your credentials before running this function
+#' @param host the api call host. Default set to espa v1 web api
+#' @param username default NULL, which fetches the username from the global environment. If defined otherwise, will run the api with the provided details
+#' @param password default NULL, which fetches the password from the global environment. If defined otherwise, will run the api with the provided details
+#'
+#' @return logical. TRUE if user is active, FALSE if credentials are wrong or API is unresponsive
+#' @export
+#'
+#' @examples # inputting the credentials
+#' espa_creds("your_espaname", "secret_password")
+#' # checking if the user is valid
+#' espa_user() # returns FALSE
+#'
+espa_user <- function(host = 'https://espa.cr.usgs.gov/api/v1/', username = NULL, password = NULL){
+  # getting the username and password from global environment if not specified
+  if(is.null(username) | is.null(password)){
+    username = tryCatch(espa_get_creds()[1], error = function(e) stop("Please set your espa-api creds in espa_creds()"))
+    password = tryCatch(espa_get_creds()[2], error = function(e) stop("Please set your espa-api creds in espa_creds()"))
+  }
+  # Running GET for espa-api
+  result = tryCatch(GET(paste0(host, "user/"), authenticate(username, password)),error = function(e) {FALSE})
+  if(is.logical(result)){
+    cat("Cannot Connect to espa-api\n")
+    return(result)
+  }
+  # getting the role of user specified
+  user_role = fromJSON(rawToChar(result$content))$roles
+  if(length(user_role)>0){
+    result = fromJSON(rawToChar(result$content))$roles == "active"
+    cat(paste("The user", username, " is", user_role,"\n"))
+    cat("\n")
+  } else {
+    cat('Oops! Invalid username password\n')
+    result = FALSE
+  }
+  return(result)
+}
