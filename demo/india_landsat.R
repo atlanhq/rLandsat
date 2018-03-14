033e359611b5421968f0629fa67d36574fb937ae
himanshu-sikaria <himanshu.sikaria@socialcops.com>
2018-03-14 20:54:00 +0530

    adding demo script

---
+++ demo/india_landsat.R
@@ -0,0 +1,24 @@
+# ======== DEMO Script to download Landsat 8 data for India for 1st half Jan 2018 ===========
+
+# get all the product IDs for India for 1 time period
+result = landsat_search(min_date = "2018-01-01", max_date = "2018-01-16", country = "India")
+
+# inputting espa creds
+espa_creds("yourusername", "yourpassword")
+
+# getting available products
+prods = espa_products(result$product_id)
+prods = prods$master
+
+# placing an espa order
+result_order = espa_order(result$product_id, product = c("sr","sr_ndvi"),
+                          projection = "lonlat",
+                          order_note = "All India Jan 2018")
+order_id = result_order$order_details$orderid
+
+# getting order status
+durl = espa_status(order_id = order_id)
+downurl = durl$order_details
+
+# download; after the order is complete
+landsat_download(download_url = downurl$product_dload_url, dest_file = getwd())
