39413287d25e7c7ca6a0da6a4353a84ac979be84
himanshu-sikaria <himanshu.sikaria@socialcops.com>
2018-07-13 00:44:12 +0530

    world_rowpath data md added

---
+++ R/world_rowpath.R
@@ -0,0 +1,16 @@
+#' World - Country wise row path mapping
+#'
+#'
+#' @docType data
+#'
+#' @usage data("world_rowpath")
+#'
+#' @format An object of class dataframe
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data("world_rowpath")
+"world_rowpath"
